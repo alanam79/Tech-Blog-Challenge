98221b7b243eb44a6522b387a8ed9384edd483e0
alanam79 <halo2382001@gmail.com>
2022-07-08 17:49:50 -0400

    added edit and delete abilities to the blogs

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-// sign up button
+// sign up
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -29,30 +29,31 @@ document
   .querySelector(".signup-form")
   .addEventListener("submit", signupFormHandler);
 
-
 //   login button
-  async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+}
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
